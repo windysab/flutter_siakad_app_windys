85d4e9542d5a6fb2b1532e033b9bb66e1a38355c
windys <windysabtami85@gmail.com>
2023-10-25 11:15:12 +0800

    flutter fic 8

---
+++ android/app/src/main/kotlin/com/example/flutter_siakad_app_windys/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_siakad_app_windys
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_siakad_app_windys/bloc/login/login_bloc.dart';
+import 'package:flutter_siakad_app_windys/pages/auth/splash_page.dart';
+
+import 'data/datasources/auth_local_datasource.dart';
+import 'pages/mahasiswa/mahasiswa_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder<bool>(
+        future: AuthLocalDatasource().isLogin(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!) {
+            return const MahasiswaPage();
+          } else {
+            return const SplashPage();
+          }
+          // return const Scaffold(
+          //   body: Center(
+          //     child: CircularProgressIndicator(),
+          //   ),
+          // );
+        },
+      ),
+    );
+  }
+}
